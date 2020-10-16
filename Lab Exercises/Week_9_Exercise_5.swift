28c867d643a6ca238f78bf4c3a2205115e18e29e
RueLee <ruelee125@gmail.com>
2020-10-16 13:59:06 -0700

    Create Week_9_Exercise_5.swift

---
+++ Lab Exercises/Week_9_Exercise_5.swift
@@ -0,0 +1,22 @@
+//Exercise 5
+/*
+Write a program that assigned a value to a variable as a starting value and an ending value and then writes all the integers (inclusive) between those two values.
+
+Enter Start:
+5
+Enter End:
+9
+
+5
+6
+7
+8
+9
+*/
+
+let start = 12
+let end = 120
+
+for i in start...end {
+	print(n)
+}
