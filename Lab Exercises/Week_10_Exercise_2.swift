7994ec497ed6d6a97b2d3173825994a09a1233c2
RueLee <ruelee125@gmail.com>
2020-10-23 13:48:41 -0700

    Create Week_10_Exercise_2.swift

---
+++ Lab Exercises/Week_10_Exercise_2.swift
@@ -0,0 +1,9 @@
+//Exercise 2
+//Using the while statement, Write a program that writes “I will not skip the fundamentals!” N times.
+
+var n = 1
+
+while n <= 100 {
+    print("I will not skip the fundamentals!")
+    n += 1
+}
