7cad57b01853f0d3b408ef666a2137b8c3106814
RueLee <ruelee125@gmail.com>
2020-10-23 13:53:43 -0700

    Create Week_10_Exercise_3.swift

---
+++ Lab Exercises/Week_10_Exercise_3.swift
@@ -0,0 +1,11 @@
+//Exercise 3
+//Using the while statement,  Write a program that prints the odd numbers from 1 to 100.
+
+var odd = 1
+
+while 1 <= 100 {
+    if odd % 2 == 1 {
+        print(odd)
+    }
+    odd += 1
+}
