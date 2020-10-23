da04634914ea96b329bdb1fc0be22bd94839c0f6
RueLee <ruelee125@gmail.com>
2020-10-23 14:03:05 -0700

    Create Week_10_Exercise_4.swift

---
+++ Lab Exercises/Week_10_Exercise_4.swift
@@ -0,0 +1,17 @@
+/*Exercise 4
+Using the while statement 
+Write a program that prints a factorial to the screen.
+
+The factorial of a number is found by multiplying itself by all positive integers less than it (excluding 0). For example, 4 factorial, written as 4!, can be computed as follows:
+
+4! == 4 * 3 * 2 * 1 == 24*/
+
+var num = 4
+var i = 4
+var factorial = 1
+while i > 1 {
+    factorial *= i
+	i -= 1
+}
+
+print("\(num)! = \(factorial)")
