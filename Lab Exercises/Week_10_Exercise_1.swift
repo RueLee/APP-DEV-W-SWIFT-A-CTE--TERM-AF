dbc80d197c7d608fd9d4b4db4de6f6f2e50b6f37
RueLee <ruelee125@gmail.com>
2020-10-23 13:42:59 -0700

    Create Week_10_Exercise_1.swift

---
+++ Lab Exercises/Week_10_Exercise_1.swift
@@ -0,0 +1,9 @@
+//Exercise 1
+//Using the while statement,  Write a program that prints Hello Swift World! exactly 100 times.
+
+var num = 1
+
+while num <= 100 {
+    print("Hello Swift World!")
+    num += 1
+}
